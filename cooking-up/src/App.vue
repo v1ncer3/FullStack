14084b913d83cf96976ea1efd4083bf94f18d3d5
v1ncer3 <andersonjrignacio@gmail.com>
2024-02-12 02:49:09 -0300

    v1 - cooking up

---
+++ cooking-up/src/App.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Banner  from "./components/Banner.vue";
+  import Conteudo from "./components/Conteudo.vue";
+
+  export default{
+    components:{  Banner: Banner ,
+                  Conteudo: Conteudo
+              }
+  }
+
+</script>
+
+
+<template>
+  <Banner />
+  <Conteudo />
+</template>
+
+<style scoped>
+
+</style>
