b41575b79264e51bdb324428c7f00377e99361e4
v1ncer3 <andersonjrignacio@gmail.com>
2024-03-02 02:25:23 -0300

    COMANDA

--- bolananza/src/components/Comanda.vue
+++ bolananza/src/components/Comanda.vue
@@ -2,32 +2,45 @@
 
 //criar condicionais para as telas novas
 export default{
+  data(){
+    return{
+      idMesa: 0,
+      valorTotal: 0,
+      comandasIndividuais: []
+    }
+  }
 }
 </script>
 
 <template>
-
+  
   <div id="mesa">
+    <button class="button" >Adicionar comanda individual</button>
     <div id="mesa-container">
-      <h1>Mesa {{ 1 }}</h1>
+      <h1>Mesa {{ idMesa }}</h1>
       <div class="mesa-row">
         <p>Comanda da mesa:</p>
         <p>
-          <strong>R${{ 0 }}</strong>
+          <strong>R${{ valorTotal }}</strong>
         </p>
       </div>
       <div class="mesa-row">
         <p>Comandas individuais:</p>
-        <p>{{ 1 }}</p>
+        <p>{{ comandasIndividuais.length }}</p>
       </div>
-      <button class="button" >Adicionar valor à mesa {{ 1}}</button>
-      <button class="button" >Adicionar comanda individual</button>
+      <button class="button" >Adicionar um item</button>
+      
+      <button class="button" >Fechar a mesa</button>
 
       <div id="cadeiras">
       </div>
+      <button class="button" >Adicionar uma mesa</button>
     </div>
   </div>
 </template>
 
 <style>
+#mesa{
+  background-color: blue;
+}
 </style>
